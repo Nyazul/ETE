e760d8b8835cb69d8793a6afbe5617cd3cc28174
Nyazul <ansarinyazul2003@gmail.com>
2024-11-21 02:23:38 +0530

    Redesigned Enquire page and added Parallax effect

--- src/main/java/com/epictasteexchange/controllers/TestController.java
+++ src/main/java/com/epictasteexchange/controllers/TestController.java
@@ -1,13 +1,21 @@
 package com.epictasteexchange.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.epictasteexchange.services.EmailService;
 import com.epictasteexchange.services.ProductService;
 
 @Controller
@@ -16,6 +24,8 @@ public class TestController {
 	
 	@Autowired
 	ProductService productService;
+	@Autowired
+	EmailService emailService;
 
 	@GetMapping("")
 	public String mainpage() {
@@ -53,5 +63,58 @@ public class TestController {
 		model.addAttribute("product", productService.getProductById(ID));
 		System.out.println("Test Product Details Page");
 		return "TestProductDetails";
+	}
+	
+	@GetMapping("/enquire")
+	public String enquire(Model model) {
+		System.out.println("Test Enquire Page");
+		model.addAttribute("products", productService.getAllProducts());
+		return "TestEnquire";
+	}
+	
+	@PostMapping("/submitEnquiry")
+	public ResponseEntity<Map<String, String>> submitEnquiry(
+	        @RequestParam Map<String, String> formData) {
+
+	    // Log received data (for debugging purposes)
+	    System.out.println("Received form data: " + formData);
+
+	    String companyName = formData.get("companyName");
+        String firstName = formData.get("firstName");
+        String lastName = formData.get("lastName");
+        String email = formData.get("email");
+        String phone = formData.get("phone");
+        String formType = formData.get("formType");
+        String emailBody;
+
+        if("Sample Request".equals(formType)) {
+        	String productName = formData.get("productName");
+        	String address = formData.get("address");
+        	emailBody = String.format("Sample Request from %s %s (%s)\n" +
+                    "Company Name: %s\n" +
+                    "Email: %s\n" +
+                    "Phone: %s\n" +
+                    "Product: %s\n" +
+                    "Address: %s\n",
+                 firstName, lastName, email, companyName, email, phone, productName, address);
+
+        } else {
+        	String query = formData.get("query");
+        	emailBody = String.format("Sample Request from %s %s (%s)\n" +
+                    "Company Name: %s\n" +
+                    "Email: %s\n" +
+                    "Phone: %s\n" +
+                    "Query: %s\n",
+                 firstName, lastName, email, companyName, email, phone, query != null ? query : "N/A");
+        }
+        String toMail = "info.epictasteexchange@gmail.com";
+
+		emailService.sendSimpleEmail(toMail, formType, emailBody);
+
+	    // Create response message
+	    Map<String, String> response = new HashMap<>();
+	    response.put("message", "Submission successful!");
+
+	    return ResponseEntity.ok(response);
 	}
 }
--- src/main/webapp/WEB-INF/views/TestHome.jsp
+++ src/main/webapp/WEB-INF/views/TestHome.jsp
@@ -72,7 +72,7 @@
 					<li><a href="#about">About Us</a></li>
 					<li><a href="/test/career">Career</a></li>
 					<li><a href="/test/products">Products</a></li>
-					<li><a href="/enquire">Enquire Now</a></li>
+					<li><a href="/test/enquire">Enquire Now</a></li>
 					<li><div class="search-bar">
 				            <form id="searchForm" action="/products/search" method="get" autocomplete="off">
 					            <input type="text" name="query" placeholder="  Search products"
