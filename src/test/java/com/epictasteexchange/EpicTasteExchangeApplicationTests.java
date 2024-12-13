1f4b73cb3d7afd2e017959c564cb0c4409c40807
nyazul <ansarinyazul2003.com>
2024-12-13 22:23:41 +0530

    Fixed Test Classes Created .jar file for application deployment

--- src/test/java/com/epictasteexchange/EpicTasteExchangeApplicationTests.java
+++ src/test/java/com/epictasteexchange/EpicTasteExchangeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bcfoods;
+package com.epictasteexchange;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BcFoodsApplicationTests {
+class EpicTasteExchangeApplicationTests {
 
 	@Test
 	void contextLoads() {
